47e1e17f0ebda752ed4c960caf47f308571a8918
Kalyan Misal <kmisal32@gmail.com>
2022-12-24 20:04:19 +0530

    new_file_added

--- src/test/java/testClasses/TC_001Login.java
+++ src/test/java/testClasses/TC_001Login.java
@@ -3,9 +3,11 @@ package testClasses;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import configureClass.PathConfigure;
+import pomClass.LoginPage;
 
 public class TC_001Login {
 
@@ -14,11 +16,20 @@ public class TC_001Login {
 	@BeforeClass
 	public void launchBrowser() {
 
-		System.setProperty("webdriver.chrome.driver", "D:\\New folder\\misal\\src\\main\\resources\\driver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver",
+				"D:\\New folder\\misal\\src\\main\\resources\\driver\\chromedriver.exe");
 
 		driver = new ChromeDriver();
 
 		driver.get(PathConfigure.facebookLogin);
 	}
 
+	@Test
+	public void tc_001() {
+		LoginPage lo = new LoginPage(driver);
+		lo.enterUserId("Tester");
+		lo.enterPassword("Demo@123");
+		lo.clickOnLogin();
+
+	}
 }
